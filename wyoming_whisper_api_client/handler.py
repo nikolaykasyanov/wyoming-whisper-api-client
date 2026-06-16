c6ae83cfec7d11da3f8b36e015dcb1360746952f
Nikolai Kasyanov <nikolay.kasyanov@me.com>
2026-06-16 18:32:13 +0200

    Cater for optionality of Transcribe.language

--- wyoming_whisper_api_client/handler.py
+++ wyoming_whisper_api_client/handler.py
@@ -59,13 +59,13 @@ class WhisperAPIEventHandler(AsyncEventHandler):
                         wavfile.setparams((1, 2, 16000, 0, 'NONE', 'NONE'))
                         wavfile.writeframes(self.audio)
 
-                        # A Transcribe event must always precede AudioStop, meaning the language should've been set by now.
-                        assert self.language
-
-                        files = {
+                        files: dict = {
                             "file": tmpfile.getvalue(),
-                            "language": self.language,
                         }
+
+                        if self.language:
+                            files["language"] = self.language
+
                         params = {
                             "temperature": "0.0",
                             "temperature_inc": "0.2",
